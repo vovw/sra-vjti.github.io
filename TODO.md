7b7eee6aacf23039bc69a3b974bc609de5720039
vovw <voidz7447@gmail.com>
2025-06-19 01:52:05 +0530

    fasterrrr baby

---
+++ TODO.md
@@ -0,0 +1,16 @@
+- landing with ideate/innovate/inspire (change font fast as fuck)
+- change the navbar completely 
+- achievement page looking sexier
+- better project page with ongoing 
+- grand sponsor 
+- soccials
+- map with all the distributin of alumni reach
+- better contact page more creatvie more joy
+- donate 
+
+
+
+
+ideate - projects
+innovate - achievements
+inspire  -  alumni
--- next.config.js
+++ next.config.js
@@ -1,15 +1,11 @@
-const withTM = require('next-transpile-modules')(['emailjs-com', 'raw-loader']); // pass the modules you would like to see transpiled
-
+/** @type {import('next').NextConfig} */
 module.exports = {
   trailingSlash: true,
-  ...withTM({
-    webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-      config.module.rules.push({
-        test: /\.md$/i,
-        loader: 'raw-loader',
-      });
-
-      return config;
-    },
-  }),
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.md$/i,
+      use: 'raw-loader',
+    });
+    return config;
+  },
 };
